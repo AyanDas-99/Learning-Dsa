05adc72467bd752fcb2481966c9df5587dcc60c1
Ayan Das <ayandas9531@gmail.com>
2022-06-18 14:59:03 +0530

    selection.cpp file created with selection sort algo

--- selection.cpp
+++ selection.cpp
@@ -1,2 +1,26 @@
 # include <iostream>
+# include <vector>
+
+using namespace std;
+
+
+int main(){
+	vector<int> v;
+	v = {1,4,71,62,33,0,-5};
+
+	for(int i=0;i<v.size()-1;i++){
+		int ind;
+		for(int j=i;j<v.size();j++){
+			if(v[j]<=v[i])
+				ind = j;
+		}
+		int temp = v[i];
+		v[i] = v[ind];
+		v[ind] = temp;
+	}
+	
+	for(int i=0;i<v.size();i++)
+		cout<<v[i]<<" ";	
+	return 0;
+}
 
