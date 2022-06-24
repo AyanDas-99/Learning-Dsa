f2c20945d7307a834ac11b78c4371f4d19478756
Ayan Das <ayandas9531@gmail.com>
2022-06-24 11:55:30 +0530

    Added QuickUnion-weighted.java as an improvement to Lazy approach in QuickUnion_lazy

---
+++ QuickUnion_weighted.java
@@ -0,0 +1,41 @@
+
+class UnionFind {
+    private int[] id;
+    private int[] size;
+    public UnionFind(int N) {
+        id = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int root(int i) {
+        while (id[i] != i)
+            i = id[i];
+        return i;
+    }
+
+    public void Union(int p, int q) {
+        int p_root = root(p);
+        int q_root = root(q);
+        if(size[q_root]<size[p_root])
+        id[q_root] = p_root;
+        else id[p_root] = q_root;
+    }
+
+    public boolean Connected(int p, int q) {
+        return root(p) == root(q);
+    }
+}
+
+public class QuickUnion_Lazy {
+    public static void main(String[] args) {
+        UnionFind a = new UnionFind(10);
+        a.Union(9, 2);
+        a.Union(4, 3);
+        a.Union(6, 5);
+        a.Union(9, 4);
+        System.out.println(a.Connected(3, 2));
+    }
+}
