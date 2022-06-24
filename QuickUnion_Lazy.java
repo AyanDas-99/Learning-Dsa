27ae15299401c022184f1e2eb1bd4253ba7e7695
Ayan Das <ayandas9531@gmail.com>
2022-06-24 11:45:35 +0530

    Added file QuickUnion.java implementing Lazy Approach

---
+++ QuickUnion_Lazy.java
@@ -0,0 +1,38 @@
+
+class UnionFind {
+    private int[] id;
+
+    public UnionFind(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int root(int i) {
+        while (id[i] != i)
+            i = id[i];
+        return i;
+    }
+
+    public void Union(int p, int q) {
+        int p_root = root(p);
+        int q_root = root(q);
+        id[q_root] = p_root;
+    }
+
+    public boolean Connected(int p, int q) {
+        return root(p) == root(q);
+    }
+}
+
+public class QuickUnion_Lazy {
+    public static void main(String[] args) {
+        UnionFind a = new UnionFind(10);
+        a.Union(9, 2);
+        a.Union(4, 3);
+        a.Union(6, 5);
+        a.Union(9, 4);
+        System.out.println(a.Connected(3, 2));
+    }
+}
